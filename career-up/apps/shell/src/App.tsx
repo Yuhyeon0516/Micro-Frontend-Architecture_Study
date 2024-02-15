6cbb1562c57b432c6cbe1f03c4f8c81af2a26737
Yuhyeon0516 <rladbgus0516@naver.com>
2024-02-15 22:20:26 +0900

    react-router-dom 을 통한 내비게이션 구현

--- career-up/apps/shell/src/App.tsx
+++ career-up/apps/shell/src/App.tsx
@@ -1,29 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import Router from "./router";
 
-import "@career-up/ui-kit/index.css";
-import { Button, Icons } from "@career-up/ui-kit";
-import { test } from "@career-up/shell-router";
+const App = () => <Router />;
 
-test();
-
-const App = () => (
-    <div className="container">
-        <div>Name: shell</div>
-        <div>Framework: react</div>
-        <div>Language: TypeScript</div>
-        <div>CSS: Empty CSS</div>
-        <div>
-            <Button>Hello</Button>
-        </div>
-        <div>
-            <Icons.Briefcase />
-        </div>
-    </div>
-);
-const rootElement = document.getElementById("app");
-if (!rootElement) throw new Error("Failed to find the root element");
-
-const root = ReactDOM.createRoot(rootElement as HTMLElement);
-
-root.render(<App />);
+export default App;
--- career-up/packages/shell-router/src/index.ts
+++ career-up/packages/shell-router/src/index.ts
@@ -1,1 +1,7 @@
-export function test() {}
+import AppRoutingManager from "./components/app-routing-manager";
+import useShellEvent from "./hooks/use-shell-event";
+import { injectFactory } from "./injector";
+
+export { useShellEvent, injectFactory, AppRoutingManager };
+
+export type * from "./types";
